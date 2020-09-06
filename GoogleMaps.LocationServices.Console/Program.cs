22fd9fb529111556bc3f303b6a675e4bfaeb6ffc
MTBoutiqaat <m.eltorri@boutiqaat.com>
2020-09-06 20:14:00 +0300

    Add project files.

---
+++ GoogleMaps.LocationServices.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GoogleMaps.LocationServices.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressData[] addresses = new AddressData[] 
+            {
+                new AddressData // Belgium
+                {
+                    Address = "Rue du Cornet 6",
+                    City = "VERVIERS",
+                    State = null,
+                    Country = "Belgium",
+                    Zip = "B-4800"
+                },
+                new AddressData
+                {
+                    Address = "1600 Pennsylvania ave",
+                    City = "Washington",
+                    State = "DC"
+                }
+            };
+
+            var gls = new GoogleLocationService();
+            foreach (var address in addresses)
+            {
+                var latlong = gls.GetLatLongFromAddress(address);
+                var Latitude = latlong.Latitude;
+                var Longitude = latlong.Longitude;
+                Console.WriteLine("Address ({0}) is at {1},{2}", address, Latitude, Longitude);
+            }
+            Console.ReadLine();
+        }
+    }
+}
